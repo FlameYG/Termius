75c85f905f6fb3215ee4639e425d0ec53617d303
yg <Flame@instance-1.us-central1-c.c.iconic-balm-345512.internal>
2022-04-04 14:41:56 +0000

    CMake install

---
+++ CMakeFiles/CMake_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMake_test.dir/test.c.o"
+  "CMake_test"
+  "CMake_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMake_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
